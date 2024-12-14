5e6599d04253caf384b3d538e2fcc8820a293acd
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-14 16:56:32 -0300

    Add client and server validation for empty note

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,11 +6,22 @@ $config = require 'config.php';
 $db = new Database($config['database']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $query = "INSERT INTO notes(body, user_id) VALUES(:body, :user_id)";
-  $db->query($query, [
-    'body' => $_POST['body'],
-    'user_id' => 3
-  ]);
+
+  $errors = [];
+
+  if (strlen($_POST['body']) === 0) {
+    $errors['body'] = "A body is required";
+  }
+
+  if (empty($errors)) {
+    $query = "INSERT INTO notes(body, user_id) VALUES(:body, :user_id)";
+    $db->query($query, [
+      'body' => $_POST['body'],
+      'user_id' => 3
+    ]);
+  }
+
+
 }
 
 require 'views/note-create.view.php';
