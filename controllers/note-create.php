2475e87ddaa2ae13718fe35658891b405f823a6d
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-14 16:28:13 -0300

    Insert note into database when submitted

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,6 +2,17 @@
 
 $heading = "Create a Note"; 
 
+$config = require 'config.php';
+$db = new Database($config['database']);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $query = "INSERT INTO notes(body, user_id) VALUES(:body, :user_id)";
+  $db->query($query, [
+    'body' => $_POST['body'],
+    'user_id' => 3
+  ]);
+}
+
 require 'views/note-create.view.php';
 
 ?>
