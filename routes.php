c80a0e7a82ecd7fd1fde27d65d41ebf70b16bc1e
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-14 15:02:55 -0300

    Refactor routes into its own file

---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  '/' => 'controllers/home.php',
+  '/about' => 'controllers/about.php',
+  '/contact' => 'controllers/contact.php',
+  '/notes' => 'controllers/notes.php',
+  '/note' => 'controllers/note.php'
+];
+
+
+?>
