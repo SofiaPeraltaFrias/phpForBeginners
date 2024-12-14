fa2b05c7f449cec372a63e832b56808414780c32
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-14 09:50:01 -0300

    Add About and Contact pages controllers and views

--- views/contact.view.php
+++ views/contact.view.php
@@ -19,9 +19,9 @@
             <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Home</a>
+                <a href="/" class="text-gray-300 rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Home</a>
                 <a href="/about" class="text-gray-300 rounded-md px-3 py-2 text-sm font-medium " aria-current="page">About</a>
-                <a href="/contact" class="text-gray-300 rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Contact</a>
+                <a href="/contact" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">Contact</a>
                 </div>
             </div>
             </div>
