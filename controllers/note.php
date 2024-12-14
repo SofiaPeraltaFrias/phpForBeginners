0c9348f10ee6d56f717a87b11e8c654443b14be4
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-14 10:57:44 -0300

    Refactor authorization from note to functions

--- controllers/note.php
+++ controllers/note.php
@@ -10,10 +10,7 @@ $db = new Database($config['database']);
  
 $note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->findOrFail();
 
-if ($note['user_id'] !== $currentUserId) {
-  abort(403);
-}
-
+authorize($note['user_id'] === $currentUserId);
 
 require "views/note.view.php";
 
--- functions.php
+++ functions.php
@@ -4,4 +4,10 @@ function urlIs($url) {
     return $_SERVER['REQUEST_URI'] === $url;
 }
 
+function authorize($condition, $status = 403) {
+  if (!$condition) {
+    abort($status);
+  }
+}
+
 ?>
