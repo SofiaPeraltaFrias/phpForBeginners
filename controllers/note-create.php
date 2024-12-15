d495f28303d94afbcb8b03773e0f3406135ba93d
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-15 07:51:13 -0300

    Add maximum note length and remove whitespace post

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -5,12 +5,18 @@ $heading = "Create a Note";
 $config = require 'config.php';
 $db = new Database($config['database']);
 
+$maxNoteLength = 30;
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   $errors = [];
 
-  if (strlen($_POST['body']) === 0) {
+  $noteLength = strlen(trim($_POST['body']));
+
+  if ($noteLength === 0) {
     $errors['body'] = "A body is required";
+  } elseif ($noteLength > $maxNoteLength) {
+    $errors['body'] = "You have exceeded the maximum note length by " . ($noteLength - $maxNoteLength) . " characters.";
   }
 
   if (empty($errors)) {
