9de021049941872b1837d17d4cc935c66a06913d
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-15 11:02:18 -0300

    Add delete note functionality

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -2,19 +2,34 @@
 
 use Core\Database;
 
-$currentUserId = 3;
+$currentUserId = 5;
 
 $config = require basepath('config.php');
-
 $db = new Database($config['database']);
- 
-$note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  //POST REQUEST, DELETE CURRENT NOTE
 
-view('notes/show.view.php', [
-  'heading' => 'Note',
-  'note' => $note
-]);
+  $note = $db->query("select * from notes where id = :id", ['id' => $_POST['id']])->findOrFail();
+  
+  authorize($note['user_id'] === $currentUserId);
+
+  $db->query("delete from notes where id = :id", ['id' => $_POST['id']]);
+  
+  header('location: /notes');
+  exit();
+
+} else {
+  //GET REQUEST, DISPLAY NOTE
+   
+  $note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->findOrFail();
+  
+  authorize($note['user_id'] === $currentUserId);
+  
+  view('notes/show.view.php', [
+    'heading' => 'Note',
+    'note' => $note
+  ]);
+}
 
 ?>
