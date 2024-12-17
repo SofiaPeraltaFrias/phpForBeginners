a6506b0bc412db1342fa26fa8e77b54b8e6c19c7
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-17 12:47:38 -0300

    Add password hashing and user greeting

--- views/index.view.php
+++ views/index.view.php
@@ -9,7 +9,7 @@
 
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <p>Welcome to the PHP for Beginners home page.</p>
+        <p>Hello <?= $_SESSION['user']['email'] ?? 'Guest' ?>, welcome to the home page. </p>
       </div>
     </main>
   </div>
