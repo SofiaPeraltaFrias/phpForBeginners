4fdb59ec844ad627b9c617f2571a8b25f2f4f355
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-17 12:00:06 -0300

    Add middleware for restricting access to pages

--- routes.php
+++ routes.php
@@ -3,10 +3,9 @@
 $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
-$router->get('/notes', 'controllers/notes/index.php');
 
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
-
 $router->delete('/note', 'controllers/notes/destroy.php');
 
 $router->get('/notes/edit', 'controllers/notes/edit.php');
@@ -16,7 +15,7 @@ $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
 
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
 
 ?>
